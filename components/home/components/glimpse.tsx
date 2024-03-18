1e2b192f4f641098b1e1d03c377281a3f53b6cfc
indramhaarta <indramhrt@gmail.com>
2024-03-18 22:30:57 +0700

    fix: resolve issue when data is null

--- components/home/components/glimpse.tsx
+++ components/home/components/glimpse.tsx
@@ -44,54 +44,58 @@ const Glimpse: FC<GlimpseProps> = ({ testimonies }) => {
             line1="Glimpse of"
             line2="Our Satisfied Clients"
           />
-          <div>
-            <Swiper
-              loop={true}
-              centeredSlides={true}
-              modules={[Navigation, Pagination, A11y, EffectFade, Autoplay]}
-              slidesPerView={slidesPerView}
-              initialSlide={0}
-              navigation
-              autoplay={{ delay: 3000 }}
-              pagination={{ clickable: true }}
-              scrollbar={{ draggable: true }}
-              onSlideChange={(e) => setActiveIndex(e.realIndex)}
-              className="h-full w-full max-w-[1200px] relative"
-            >
-              {testimonies.map((testimony, id) => (
-                <div className="!h-[350px]" key={`${id} ${testimony.brand}`}>
-                  <SwiperSlide
-                    className={`!flex !justify-center w-full items-center mb-8 ${id === activeIndex && "z-[1]"}`}
-                    key={`${id} ${testimony.brand}`}
-                  >
-                    <div
-                      className={`flex !h-[300px] aspect-square p-12 ${id === activeIndex && "!p-2"}`}
+          {testimonies.length > 0 && (
+            <div>
+              <Swiper
+                loop={true}
+                centeredSlides={true}
+                modules={[Navigation, Pagination, A11y, EffectFade, Autoplay]}
+                slidesPerView={slidesPerView}
+                initialSlide={0}
+                navigation
+                autoplay={{ delay: 3000 }}
+                pagination={{ clickable: true }}
+                scrollbar={{ draggable: true }}
+                onSlideChange={(e) => setActiveIndex(e.realIndex)}
+                className="h-full w-full max-w-[1200px] relative"
+              >
+                {testimonies.map((testimony, id) => (
+                  <div className="!h-[350px]" key={`${id} ${testimony.brand}`}>
+                    <SwiperSlide
+                      className={`!flex !justify-center w-full items-center mb-8 ${id === activeIndex && "z-[1]"}`}
+                      key={`${id} ${testimony.brand}`}
                     >
-                      <Image
-                        src={getImageUrl(testimony.brand_logo)}
-                        width={id === activeIndex ? 350 : 250}
-                        height={id === activeIndex ? 350 : 250}
-                        className="object-cover"
-                        alt={testimony.brand}
-                      />
-                    </div>
-                  </SwiperSlide>
-                </div>
-              ))}
-            </Swiper>
-          </div>
-          <div className="hidden md:block">
-            <div className="pt-8">
-              <p className="font-semibold italic text-center">
-                {testimonies[activeIndex].heading}
-              </p>
+                      <div
+                        className={`flex !h-[300px] aspect-square p-12 ${id === activeIndex && "!p-2"}`}
+                      >
+                        <Image
+                          src={getImageUrl(testimony.brand_logo)}
+                          width={id === activeIndex ? 350 : 250}
+                          height={id === activeIndex ? 350 : 250}
+                          className="object-cover"
+                          alt={testimony.brand}
+                        />
+                      </div>
+                    </SwiperSlide>
+                  </div>
+                ))}
+              </Swiper>
             </div>
-            <div className="pt-8 w-full flex justify-center">
-              <p className="max-w-[1200px]">
-                {testimonies[activeIndex].description}
-              </p>
+          )}
+          {testimonies.length > 0 && (
+            <div className="hidden md:block">
+              <div className="pt-8">
+                <p className="font-semibold italic text-center">
+                  {testimonies[activeIndex].heading}
+                </p>
+              </div>
+              <div className="pt-8 w-full flex justify-center">
+                <p className="max-w-[1200px]">
+                  {testimonies[activeIndex].description}
+                </p>
+              </div>
             </div>
-          </div>
+          )}
         </div>
       </div>
     </div>
--- utils/getImageUrl.ts
+++ utils/getImageUrl.ts
@@ -1,4 +1,5 @@
-const getImageUrl = (src: string) => {
+const getImageUrl = (src: string | null | undefined) => {
+  if (!src) return "";
   return `${process.env.NEXT_PUBLIC_API_BACKEND_URL}/${src.split("/").slice(3).join("/")}`;
 };
 
